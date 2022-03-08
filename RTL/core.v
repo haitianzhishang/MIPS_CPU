b47b473b945eef62b079913c89b9e67813845c58
chen xiaowei <chen1408@e.ntu.edu.sg>
2022-03-08 18:19:14 +0800

    -F Add core and alu module and to be done

---
+++ RTL/core.v
@@ -0,0 +1,38 @@
+//--------------------------------------------------------------------
+// Design Name : core
+// File Name   : core.sv
+// Function    :  
+// Designer    : Chen Xiaowei         - Nanyang technological University 
+//                                    - Technische Universität München
+// Email       : chen1408@e.ntu.edu.sg
+// Blog         : haitianzhishang.github.io
+//---------------------------------------------------------------------
+module core #(
+  paramater  
+)
+(
+  input             clk,    
+  input             rst_n, 
+  input    [  :  ]    ,
+  output   [  :  ]    ,
+  
+);
+
+always@(posedge clk or negedge rst) 
+begin
+  if(!rst) 
+  begin
+    
+  end
+  else
+  begin
+    
+  end
+end
+
+always@(*) 
+begin
+
+end
+
+endmodule : core
